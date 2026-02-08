b4359ed4758878799ec421e507993f2fd383a69c
Roniel C. Carbon <cuaresmaroniel27@gmail.com>
2026-02-08 17:34:58 +0800

    feat: add a prototype of landing page and installing firebase

---
+++ CONTROLLERS/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBeWjRz4WZJiJMVWfw2HKznC-A8lyOsZ_Q",
+authDomain: "aquaroute-485505. firebaseapp.com",
+projectId: "aquaroute-485505",
+storageBucket: "aquaroute-485505.firebasestorage.app",
+messagingSenderId: "270328154243",
+appId: "1:270328154243:web: c5712666bf4f6d497c73b6"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
